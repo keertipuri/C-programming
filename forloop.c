84b530b220989fd2533ea87a15f966cebfaef033
Keerti Puri <146538963+keertipuri@users.noreply.github.com>
2023-10-30 17:05:27 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,12 @@
+//program to display even numbers from0 to 14
+#include<stdio.h>
+int main()
+{
+    int i;
+    i=0;
+    for(i=0;i<=14;i+=2)
+    {
+      printf("%d\n",i);
+    }
+    return 0;
+}
